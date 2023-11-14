783eb2910cf874392fd4203bdaa5d2a1622d3375
OlexanderSolomko <150425563+OlexanderSolomko@users.noreply.github.com>
2023-11-14 12:05:06 +0200

    Add files via upload

--- rank/rank.py
+++ rank/rank.py
@@ -1,20 +1,26 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-gdf = gpd.read_file('rank.geojson')
+def load_and_filter_data(file_path, country):
+    gdf = gpd.read_file(file_path)
+    return gdf[gdf['country'] == country]
 
-gdf = gdf[gdf['country'] == 'United States']
-gdf = gdf.sort_values(by='national_rank')
+def plot_top_n_universities(gdf, n=10, crs='epsg:4326', figsize=(12, 6), cmap='coolwarm'):
+    top_n_universities = gdf.sort_values(by='national_rank').head(n)
+    top_n_universities = top_n_universities.to_crs(crs)
 
-top_10_us_universities = gdf.head(10)
+    fig, ax = plt.subplots(figsize=figsize)
+    top_n_universities.plot(column='national_rank', cmap=cmap, linewidth=0.8, ax=ax, legend=True)
 
-top_10_us_universities = top_10_us_universities.to_crs('epsg:4326')
+    for x, y, label in zip(top_n_universities.geometry.centroid.x, top_n_universities.geometry.centroid.y, top_n_universities['university_name']):
+        ax.text(x, y, label, fontsize=8, ha='left')
 
-fig, ax = plt.subplots(figsize=(12, 6))
-top_10_us_universities.plot(column='national_rank', cmap='coolwarm', linewidth=0.8, ax=ax, legend=True)
+    ax.set_title(f'Top {n} Universities in the USA by National Ranking')
+    plt.show()
 
-for x, y, label in zip(top_10_us_universities.geometry.centroid.x, top_10_us_universities.geometry.centroid.y, top_10_us_universities['university_name']):
-    ax.text(x, y, label, fontsize=8, ha='left')
+if __name__ == "__main__":
+    file_path = 'rank.geojson'
+    target_country = 'United States'
 
-ax.set_title('Top 10 Universities in the USA by National Ranking')
-plt.show()
+    universities_data = load_and_filter_data(file_path, target_country)
+    plot_top_n_universities(universities_data)
